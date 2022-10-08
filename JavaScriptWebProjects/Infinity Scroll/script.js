111ca2b0af257f647c8fbaedb01bfe25fd26cdf0
doVa (Doğukan Variş) <varisdogukan@outlook.com>
2022-10-08 17:45:18 +0300

    Added Infinite Scroll project

---
+++ JavaScriptWebProjects/Infinity Scroll/script.js
@@ -0,0 +1,97 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+let isInitialLoad = true;
+
+// Unsplash API
+let initialCount = 5;
+const apiKey = "Ib9eXDFqDh-staIXN1Xa0AIhRnh0ykO_qsAwe9-9SiQ";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${initialCount}`;
+
+function updateAPIURLWithNewCount(picCount) {
+  apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${picCount}`;
+}
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to ling to Unsplash
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    // Create <img> for photo
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.description,
+      title: photo.description,
+    });
+
+    // Event Listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
+
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// pushlamadan önce api keyi dene netlifyda dene
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+
+    displayPhotos();
+
+    if (isInitialLoad) {
+      updateAPIURLWithNewCount(10);
+      isInitialLoad = false;
+    }
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+// On Load
+getPhotos();
--- JavaScriptWebProjects/README.md
+++ JavaScriptWebProjects/README.md
@@ -1,7 +1,13 @@
 # 20 JavaScript Web Projects
 
-## `1 - Quote Generator Project`
+## `1 - QUOTE GENERATOR`
 
 ##### Using Html,Css and JavaScript [LIVE-CODE-URL](https://ztm-quote-generator-dova.netlify.app/) -->
 
 ![](images/Quote-Generator.png)
+
+## `2 - INFINITE SCROLL`
+
+##### Using Html,Css and JavaScript [LIVE-CODE-URL](https://ztm-infinity-scroll-dova.netlify.app/) -->
+
+![](images/infinite-scroll.png)
